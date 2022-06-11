aa41466e11ce9ce39a0934a1999e68e971c38b60
Dominika Szymańska <dominika1872@gmail.com>
2022-06-12 01:09:35 +0200

    Add 404 view

---
+++ views/404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head></head>
+  <?php require "./layout/head.php"; ?>
+  <title>Nie znaleziono strony</title>
+</head>
+<body>
+  <h1>404 - Page not found</h1>
+  <a href="/">Wróć do strony głównej</a>
+</body>
+</html>
